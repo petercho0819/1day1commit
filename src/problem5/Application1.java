7ddc841759c1943c3176d7a318d66b4d085ec043
petercho0819 <dongr52@naver.com>
2021-02-22 23:19:35 +0900

    210222 commit

--- src/problem5/Application1.java
+++ src/problem5/Application1.java
@@ -1,10 +1,77 @@
 package problem5;
 
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// 구글 입사문제
+//		8이 포함되어 있는 숫자의 갯수를 카운팅 하는 것이 아니라 8이라는 숫자를 모두 카운팅 해야 한다.
+//		(※ 예를들어 8808은 3, 8888은 4로 카운팅 해야 함)
+		
+		/* 접근 방법 */
+		/* 1 ~ 10,000으로 제한
+		 * 1부터 10000까지 for문을 돌린다
+		 *  
+		 * 방법 1.
+		 * 10으로 나눠서 나머지가 8이면 
+		 * 
+		 * 
+		 * 방법2.
+		 * 1 ~10000까지 for문으로 돌린다
+		 * String 값으로 받는다
+		 * String을 하나씩 뜯어서 8이 있으면 카운트를 한다.
+		 * 
+		 * 
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		int count = 0;
+		String sNum = "";
+		for(int i = 1; i <= num; i++ ) {
+			 sNum = String.valueOf(i);	// 입력받은 숫자를 문자열로 전환		
+			for(int j = 0; j < sNum.length() ; j++) {
+			
+				if(sNum.charAt(j) == '8') {	// 입력받은 문자열을 하나씩 쪼개서 8이 있으면 카운트
+					
+					count++;
+				}
+//				System.out.println("j" + j);
+			
+			}
+//			
+		}
+		
+		System.out.println("result " + count);
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/problem5/Application3.java
@@ -0,0 +1,47 @@
+package problem5;
+
+import java.util.Scanner;
+
+public class Application3 {
+	public static void main(String[] args) {
+//	자기 자신을 제외한 모든 양의 약수들의 합이 자기 자신이 되는 자연수를 완전수라고 한다. 
+//	예를 들면, 6과 28은 완전수이다. 
+//	6=1+2+3 // 1,2,3은 각각 6의 약수 28=1+2+4+7+14 //
+//			1,2,4,7,14는 각각 28의 약수
+//			입력으로 자연수 N을 받고, 출력으로 N 이하의 모든 완전수를 출력하는 코드를 작성하라
+//
+		/* 접근법 */
+		/*
+		 * 1. 양의 정수를 입력받는다
+		 * 2. for문을 통해 나누어서 나머지가 0인 수를 찾는다
+		 * 3. 나머지가 0인수를 더한 값이 입력한 양의 정수와 동일하면 완전수 이다
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요 ");
+		int num = sc.nextInt();
+		
+		for(int i = 0; i < num; i++) {
+			if(num % num == 0 ) {
+				
+				int num2 += num;
+				
+				
+			}else {
+				
+				System.out.println("완전수가 아니다");
+			}
+		}
+		
+		
+	}
+	
+	
+	
+	
+
+
+
+
+
+}
